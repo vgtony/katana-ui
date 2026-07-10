d4c84df6134fc42fcd5157bc35e71d490b3c3425
Antonis Vasilakos <109095599+vgtony@users.noreply.github.com>
2026-07-10 14:02:15 +0300

    feat: update environment configuration for local-network; remove production environment files

--- katana-slice-manager/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-import { buildKatanaApiEndpoints } from './api-endpoints';
-
-const apiBase = '/api';
-
-export const environment = {
-  production: true,
-  apiBase,
-  endpoints: buildKatanaApiEndpoints(apiBase)
-};
--- katana-slice-manager/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-import { buildKatanaApiEndpoints } from './api-endpoints';
-
-const apiBase = 'http://localhost:8000/api';
-
-export const environment = {
-  production: false,
-  apiBase,
-  endpoints: buildKatanaApiEndpoints(apiBase)
-};
